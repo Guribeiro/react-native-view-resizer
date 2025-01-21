480557b5b3f4ce3089505d0ac69aabc3158bfb49
Gustavo Henrique <gugahribeiro@hotmail.com>
2025-01-21 12:14:13 -0300

    chore: add definition types

--- src/resizer.d.ts
+++ src/resizer.d.ts
@@ -1,5 +1,5 @@
 import type { FC } from 'react';
-import type { ResizeButtonProps as ButtonProps, ResizerProps as Props } from './index'
+import type { ResizeButtonProps as ButtonProps, ResizerProps as Props } from './types'
 
 declare module 'react-native-view-resizer' {
   export interface ResizerProps extends Props { }
